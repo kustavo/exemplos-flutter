e3af49cfc2bb2134648e3be999cfb772364d6dca
Gustavo Araujo <gustavo.araujo@squadra.com.br>
2021-06-28 21:45:17 -0300

    update

---
+++ projeto-perguntas-udemy-cod3r/README.md
@@ -0,0 +1,19 @@
+# Aprenda Flutter e Desenvolva Apps Para Android e IOS 2021
+
+Um Curso Completo Para Aprender Google Flutter e Dart Para a Construção de Apps Nativas Para iOS e Android.
+
+## Instrutor
+
+Leonardo Moura Leitao
+
+## Instituição
+
+Cod3r Cursos Online
+
+## Projeto
+
+Projeto perguntas e respostas
+
+## Link
+
+https://www.udemy.com/share/102BJdAkEZcVtQTHo=/
---
+++ projeto-perguntas-udemy-cod3r/lib/resultado.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReinicarQuestionario;
+
+  const Resultado(this.pontuacao, this.quandoReinicarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns';
+    } else if (pontuacao < 12) {
+      return 'Você é bom!';
+    } else if (pontuacao < 16) {
+      return 'Impressionante!';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(child: Text(fraseResultado, style: TextStyle(fontSize: 28))),
+        TextButton(
+          child: Text(
+            'Reiniciar',
+            style: TextStyle(fontSize: 18),
+          ),
+          onPressed: quandoReinicarQuestionario,
+        )
+      ],
+    );
+  }
+}
